16c95338ee02ecff2df709fc13fe72694baa80a2
Pavlo Bzdel <pavlobzdel@gmail.com>
2021-12-02 19:30:56 +0200

    added modal window

--- js/index.js
+++ js/index.js
@@ -24,4 +24,32 @@
         menuItem.addEventListener("click", toggleMenu);
       }
     )
+
+  let modal = document.getElementById("myModal");
+  let btn = document.getElementById("myBtn");
+  let span = document.getElementsByClassName("close")[0];
+
+
+  btn.onclick = function() {
+  modal.style.display = "block";
+  }
+
+
+  span.onclick = function() {
+  modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+    modal.style.display = "none";
+    }
+  }
+
+  let form = document.getElementsById("cancel__button");
+
+  window.onclick = function(event) {
+    if (event.target == form) {
+        form.style.display = "none";
+    }
+}
 })()
